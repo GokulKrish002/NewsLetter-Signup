a588ebc7fcb0dcd495736cddd48c75244dc91bf0
Gokul <itsgokul2002@gmail.com>
2022-12-27 16:52:56 +0530

    Make a new start

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const bodyParser =require("body-parser")
-const app = express();
+const bodyparser = require("body-parser");
+const app =express();
 
-
+app.use(express.static("public"));
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html");
+    res.sendFile(__dirname+"/signup.html")
 });
 
-
 app.listen(3000,function () {
-    console.log("Server is up and running in port:3000.");
+    console.log("Server is up and running in port 3000");
 });
